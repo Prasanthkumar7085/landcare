39d7e5bd92d381a8f005dfbeeab1ddcdf7f88fff
ganesh.oro <ganesh.s@orotron.com>
2024-08-05 12:56:40 +0530

    GA -- removed authorization for list markers and single marker api's

--- src/app/api/v1.0/maps/[id]/markers/[marker_id]/route.ts
+++ src/app/api/v1.0/maps/[id]/markers/[marker_id]/route.ts
@@ -1,29 +1,17 @@
 import { NextRequest } from "next/server";
 import { MarkersController } from "../../../../../../../../lib/controllers/markersController";
 import { ResponseHelper } from "../../../../../../../../lib/helpers/reponseHelper";
-import { AddMarkerSchema } from "../../../../../../../../lib/validations/markers/addMarker";
 import { validate } from "../../../../../../../../lib/middlewares/validationMiddlware";
-import { validateAccessToken } from "../../../../../../../../lib/middlewares/authMiddleware";
+import { AddMarkerSchema } from "../../../../../../../../lib/validations/markers/addMarker";
 
 const markersController = new MarkersController();
 
 
 export async function GET(req: NextRequest, { params }: any) { 
-    //Check authorization
-    const authResult: any = await validateAccessToken(req);
-    if (authResult.status === 403) {
-        return authResult
-    }
-
     return markersController.getOne(req,params);
 }
 
 export async function PATCH(req: NextRequest, { params }: any) { 
-    //Check authorization
-    const authResult: any = await validateAccessToken(req);
-    if (authResult.status === 403) {
-        return authResult
-    }
     // Validate request
     const reqData = await req.json();
     const validationErrors = await validate(AddMarkerSchema, reqData);
@@ -36,11 +24,5 @@ export async function PATCH(req: NextRequest, { params }: any) {
 
 
 export async function DELETE(req: NextRequest, { params }: any) {
-    //Check authorization
-    const authResult: any = await validateAccessToken(req);
-    if (authResult.status === 403) {
-        return authResult
-    }
-    
     return markersController.delete(req, params);
 }
