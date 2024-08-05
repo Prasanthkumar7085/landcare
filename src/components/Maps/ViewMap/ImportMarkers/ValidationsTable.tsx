bb0d1d94153e669c9ae53aaf8d9cdcd0bcbe117c
sowjanya <sowjanya.t@orotron.com>
2024-08-05 10:19:56 +0530

    add styles to validation table

--- src/components/Maps/ViewMap/ImportMarkers/ValidationsTable.tsx
+++ src/components/Maps/ViewMap/ImportMarkers/ValidationsTable.tsx
@@ -5,32 +5,38 @@ const ValidationsTable = ({ validationsData }: any) => {
 
       <h2>Not Uploaded {validationsData?.length}</h2>
       <div className="tableContainer">
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Position</th>
-            <th>Host Organisation</th>
-            <th>LLS Region</th>
-            <th>Phone</th>
-            <th>Email</th>
-            <th>Location</th>
-            <th>Postcode</th>
-            <th>Error</th>
+        <table className="table" >
+          <thead className="thead" style={{
+            height: "32px",
+            position: "sticky",
+            top: "0px",
+            zIndex: "2",
+            color: "white",
+          }}>
+            <tr className="table-row">
+              <th className="cell">Name</th>
+              <th className="cell">Position</th>
+              <th className="cell">Host Organisation</th>
+              <th className="cell">LLS Region</th>
+              <th className="cell">Phone</th>
+              <th className="cell">Email</th>
+              <th className="cell">Location</th>
+              <th className="cell">Postcode</th>
+              <th className="cell">Error</th>
           </tr>
         </thead>
-        <tbody>
+          <tbody className="tbody">
           {validationsData.map((error: any, index: any) => (
-            <tr key={index}>
-              <td>{error.name || "N/A"}</td>
-              <td>{error.position || "N/A"}</td>
-              <td>{error.host_organization || "N/A"}</td>
-              <td>{error.lls_region || "N/A"}</td>
-              <td>{error.phone || "N/A"}</td>
-              <td>{error.email.slice(0, 9) || "N/A"}</td>
-              <td>{error.location || "N/A"}</td>
-              <td>{error.post_code || "N/A"}</td>
-              <td>{error.error}</td>
+            <tr className="table-row" key={index}>
+              <td className="cell">{error.name || "N/A"}</td>
+              <td className="cell">{error.position || "N/A"}</td>
+              <td className="cell">{error.host_organization || "N/A"}</td>
+              <td className="cell">{error.lls_region || "N/A"}</td>
+              <td className="cell">{error.phone || "N/A"}</td>
+              <td className="cell">{error.email.slice(0, 9) || "N/A"}</td>
+              <td className="cell">{error.location || "N/A"}</td>
+              <td className="cell">{error.post_code || "N/A"}</td>
+              <td className="cell">{error.error}</td>
             </tr>
           ))}
         </tbody>
