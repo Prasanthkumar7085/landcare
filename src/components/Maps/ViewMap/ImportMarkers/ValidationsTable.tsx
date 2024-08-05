a5e6777bdc0813088ff80064387c9dd2e8504d9b
sowjanya <sowjanya.t@orotron.com>
2024-08-05 10:40:05 +0530

    Add styles to import validation

--- src/components/Maps/ViewMap/ImportMarkers/ValidationsTable.tsx
+++ src/components/Maps/ViewMap/ImportMarkers/ValidationsTable.tsx
@@ -1,9 +1,16 @@
+import { Typography } from "@mui/material";
+
 const ValidationsTable = ({ validationsData }: any) => {
   return (
     <div className="validationTableContainer">
-      <h2>Uploaded (9)</h2>
-
-      <h2>Not Uploaded {validationsData?.length}</h2>
+      <div className="validationHead">
+        <Typography variant="h5">Files</Typography>
+        <div className="importInfo">
+          <Typography className="uploaded">Uploaded <span>9</span> </Typography>
+          <Typography className="failed">Failed  <span>{validationsData?.length}</span></Typography>
+        </div>
+        <div></div>
+      </div>
       <div className="tableContainer">
         <table className="table" >
           <thead className="thead" style={{
@@ -23,24 +30,24 @@ const ValidationsTable = ({ validationsData }: any) => {
               <th className="cell">Location</th>
               <th className="cell">Postcode</th>
               <th className="cell">Error</th>
-          </tr>
-        </thead>
-          <tbody className="tbody">
-          {validationsData.map((error: any, index: any) => (
-            <tr className="table-row" key={index}>
-              <td className="cell">{error.name || "N/A"}</td>
-              <td className="cell">{error.position || "N/A"}</td>
-              <td className="cell">{error.host_organization || "N/A"}</td>
-              <td className="cell">{error.lls_region || "N/A"}</td>
-              <td className="cell">{error.phone || "N/A"}</td>
-              <td className="cell">{error.email.slice(0, 9) || "N/A"}</td>
-              <td className="cell">{error.location || "N/A"}</td>
-              <td className="cell">{error.post_code || "N/A"}</td>
-              <td className="cell">{error.error}</td>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className="tbody">
+            {validationsData.map((error: any, index: any) => (
+              <tr className="table-row" key={index}>
+                <td className="cell">{error.name || "N/A"}</td>
+                <td className="cell">{error.position || "N/A"}</td>
+                <td className="cell">{error.host_organization || "N/A"}</td>
+                <td className="cell">{error.lls_region || "N/A"}</td>
+                <td className="cell">{error.phone || "N/A"}</td>
+                <td className="cell">{error.email.slice(0, 9) || "N/A"}</td>
+                <td className="cell">{error.location || "N/A"}</td>
+                <td className="cell">{error.post_code || "N/A"}</td>
+                <td className="cell">{error.error}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
