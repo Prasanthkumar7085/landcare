1532dae1573a30ccb476b6cc64c6e2864b0d61ad
prasanth_oro <prasanth.m@orotron.com>
2024-08-06 16:28:39 +0530

    remove polygon in public map

--- src/components/Maps/ViewMap/PublicMap/index.tsx
+++ src/components/Maps/ViewMap/PublicMap/index.tsx
@@ -178,7 +178,7 @@ const PublicMap = () => {
     drawingManager.setMap(map);
     drawingManagerRef.current = drawingManager;
     const newPolygon = new maps.Polygon({
-      paths: polygonCoords,
+      paths: [],
       strokeColor: "#282628",
       strokeOpacity: 0.8,
       strokeWeight: 4,
