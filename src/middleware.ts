00f8854019f514479d7192f07556356da2db9c4c
prasanth_oro <prasanth.m@orotron.com>
2024-08-28 09:46:19 +0530

    login issue while view public map

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
-const protectedRoutes = ["/maps"];
+const protectedRoutes = ["/view-map", "/maps?page"];
 
 const unProtectedRoutes = ["/", "forgot-password"];
 function containsSubstring(inputString: string, substrings: Array<string>) {
@@ -15,9 +15,9 @@ const isAuthenticated = (req: NextRequest) => {
 
 export default async function middleware(req: NextRequest) {
   if (
-
     !isAuthenticated(req) &&
-    containsSubstring(req.nextUrl.pathname, protectedRoutes)&& !req.nextUrl.pathname.includes('/api')
+    containsSubstring(req.nextUrl.pathname, protectedRoutes) &&
+    !req.nextUrl.pathname.includes("/api")
   ) {
     const absoluteURL = new URL("/", req.nextUrl.origin);
     return NextResponse.redirect(absoluteURL.toString());
